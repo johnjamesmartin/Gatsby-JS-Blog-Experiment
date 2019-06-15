8d8fd0409f40bc00c352ea7e070449df10e74175
John Martin <johnjamesmartin@live.com>
2019-06-15 23:51:49 +0100

    Add ability to fetch markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,15 @@ module.exports = {
     title: "Gatsby Bootcamp",
     author: "John Martin",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
 }
---
+++ src/posts/react.md
@@ -0,0 +1,10 @@
+---
+title: "React"
+date: "2019-02-01"
+---
+
+I like React!
+
+## Topics covered
+
+1. React
